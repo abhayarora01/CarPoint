8fede94b54c46f6d8532b222b37d13e93dfa9a03
Abhay <aroraabhay390@gmail.com>
2026-06-11 02:52:56 +0530

    Reduce middleware scope

--- middleware.js
+++ middleware.js
@@ -42,11 +42,13 @@ const clerk = clerkMiddleware(async (auth, req) => {
 // Chain middlewares - ArcJet runs first, then Clerk
 export default createMiddleware(aj, clerk);
 
+
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    "/admin/:path*",
+    "/saved-cars/:path*",
+    "/reservations/:path*",
+    "/api/:path*",
   ],
 };
